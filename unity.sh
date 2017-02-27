8031b84f0109432815289627c1b3213ad03c6a56
Lili Cosic <lili@kinvolk.io>
2017-02-27 09:12:04 +0100

    workaround for chrome-sandbox

--- unity.sh
+++ unity.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-exec /app/extra/Unity/Editor/Unity
+#https://chromium.googlesource.com/chromium/src/+/37.0.2062.124/sandbox/linux/suid/client/setuid_sandbox_client.cc#236
+export CHROME_DEVEL_SANDBOX=
+
+# we need this to fake permissions
+LD_PRELOAD=/app/extra/Unity/Editor/libunity-nosuid.so /app/extra/Unity/Editor/Unity
 
